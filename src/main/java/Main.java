918c7ec39c3ce8fa54d455c7d153860341278e5d
Users <lana.chebotareva.95@mail.ru>
2018-11-11 12:30:00 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import com.sun.corba.se.spi.activation.Server;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String args[]) throws FileNotFoundException {
+
+        Singliton singliton=Singliton.getInstance();
+        System.out.println(singliton);
+
+        FileInputStream fileInputStream;
+        //инициализируем специальный объект Properties
+        //типа Hashtable для удобной работы с данными
+        Properties prop = new Properties();
+
+        try {
+            //обращаемся к файлу и получаем данные
+            fileInputStream = new FileInputStream("src/main/resources/singliton.properties");
+            prop.load(fileInputStream);
+
+            String site = prop.getProperty("site");
+            //String loginToSite = prop.getProperty("login");
+            // String passwordToSite = prop.getProperty("password");
+
+            //печатаем полученные данные в консоль
+            System.out.println(
+                    // "site: " + site
+                    "Чтение файла singliton.properties: " + prop
+                    // + "\npasswordToSite: " + passwordToSite
+            );
+
+        } catch (IOException e) {
+            System.out.println("Ошибка в программе: файл " + "resources/singliton.properties" + " не обнаружено");
+            e.printStackTrace();
+        }
+
+    }
+
+}
